1ed478d6d2015844240624b626680c81de9276d0
Ben Klein <robobenklein@gmail.com>
2016-02-10 23:44:16 -0500

    Fresh Origin start

---
+++ src/main/java/frc/team3966/toastrhino/commands/MoveToOrigin.java
@@ -0,0 +1,42 @@
+package frc.team3966.toastrhino.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team3966.toastrhino.RobotModule;
+
+public class MoveToOrigin extends Command {
+
+  public MoveToOrigin() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(RobotModule.navigation);
+    requires(RobotModule.drive);
+  }
+
+  // Called just before this Command runs the first time
+  protected void initialize() {
+    SmartDashboard.putBoolean("I am Home?", false);
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  protected void execute() {
+    
+    //RobotModule.drive.TankDrive(rspeed, lspeed);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  protected void end() {
+    RobotModule.drive.doNothing();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  protected void interrupted() {
+    RobotModule.drive.doNothing();
+  }
+}
