87b8a7a73e9b2eac3456f78f62440cba113b0d62
Ben Klein <robobenklein@gmail.com>
2016-02-16 18:13:42 -0500

    Change finished evaluation

--- src/main/java/frc/team3966/toastrhino/commands/SwitchPID.java
+++ src/main/java/frc/team3966/toastrhino/commands/SwitchPID.java
@@ -13,30 +13,36 @@ public class SwitchPID extends Command {
 
   NetworkTable GRIPtable;
 
+  private boolean finished = false;
+
   public SwitchPID() {
     requires(RobotModule.drive);
   }
 
   // Called just before this Command runs the first time
   protected void initialize() {
-    if (RobotMap.usePID) {
-      RobotMap.usePID = false;
-      SmartDashboard.putBoolean("PID Control?", RobotMap.usePID);
-      RobotModule.logger.info("PID is OFF");
-    } else {
-      RobotMap.usePID = true;
-      SmartDashboard.putBoolean("PID Control?", RobotMap.usePID);
-      RobotModule.logger.info("PID is ON");
-    }
+    finished = false;    
   }
 
   // Called repeatedly when this Command is scheduled to run
   protected void execute() {
+    if (!finished) {
+      if (RobotMap.usePID) {
+        RobotMap.usePID = false;
+        SmartDashboard.putBoolean("PID Control?", RobotMap.usePID);
+        RobotModule.logger.info("PID is OFF");
+      } else {
+        RobotMap.usePID = true;
+        SmartDashboard.putBoolean("PID Control?", RobotMap.usePID);
+        RobotModule.logger.info("PID is ON");
+      }
+      finished = true;
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
   protected boolean isFinished() {
-    return true;
+    return finished;
   }
 
   // Called once after isFinished returns true
