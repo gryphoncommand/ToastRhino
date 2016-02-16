84fe613456945aa9710c3860bbdcf596596ed0ac
Cade Brown <brown.cade@gmail.com>
2016-02-16 18:22:58 -0500

    Changed to start time instead of last time

--- src/main/java/frc/team3966/toastrhino/commands/KickOut.java
+++ src/main/java/frc/team3966/toastrhino/commands/KickOut.java
@@ -13,7 +13,7 @@ public class KickOut extends Command {
   public static final double converFactor = Math.pow(10, -9);
   public boolean doThis = true;
   public double time = 0;
-  private long lastTime = 0; //dont touch :P
+  private long startTime = 0; //dont touch :P
 
   public KickOut() {
     requires(RobotModule.arm);
@@ -22,20 +22,19 @@ public class KickOut extends Command {
   // Called just before this Command runs the first time
   protected void initialize() {
 	  doThis = true;
-	  time = System.nanoTime();
+	  startTime = System.nanoTime();
   }
 
   // Called repeatedly when this Command is scheduled to run
   protected void execute() {
     long ctime = System.nanoTime();
-    time += (ctime - lastTime) * converFactor;
+    time = (ctime - startTime) * converFactor;
     if (time >= maxTime) {
     	return;
     }
     if (doThis) {
     	//your code does this
     }
-    lastTime = ctime;
   }
 
   // Make this return true when this Command no longer needs to run execute()
