cce20c77109f1666eee72a611e2df4e7b984d115
Cade Brown <brown.cade@gmail.com>
2016-02-16 18:10:29 -0500

    added timed cmd

---
+++ src/main/java/frc/team3966/toastrhino/commands/KickOut.java
@@ -0,0 +1,53 @@
+package frc.team3966.toastrhino.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import frc.team3966.toastrhino.RobotModule;
+
+/**
+ *
+ */
+public class KickOut extends Command {
+
+  public static final double maxTime = .25;//max time, in seconds
+  public static final double converFactor = Math.pow(10, -6);
+  public boolean doThis = true;
+  public double time = 0;
+  private long lastTime = 0; //dont touch :P
+
+  public KickOut() {
+    requires(RobotModule.arm);
+  }
+
+  // Called just before this Command runs the first time
+  protected void initialize() {
+	  doThis = true;
+	  time = 0;
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  protected void execute() {
+    long ctime = System.nanoTime();
+    time += (ctime - lastTime) * converFactor;
+    if (time >= maxTime) {
+    	return;
+    }
+    if (doThis) {
+    	//your code does this
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  protected boolean isFinished() {
+    return !doThis;
+  }
+
+  // Called once after isFinished returns true
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  protected void interrupted() {
+  }
+}
--- src/main/java/frc/team3966/toastrhino/util/Values.java
+++ src/main/java/frc/team3966/toastrhino/util/Values.java
@@ -1,14 +1,12 @@
 package frc.team3966.toastrhino.util;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.team3966.toastrhino.RobotModule;
 
 //use this class to get preferences and user set values at runtime
 public class Values {
 
-	@SuppressWarnings("deprecation")
 	public static double getGenericDouble(String path) { //returns the key for "path" var in user set values. Returns -0.0 if nothing exists
-		return SmartDashboard.getDouble(path);
+		return SmartDashboard.getNumber(path, -0.0d);
 	}
 
 	public static double getAngleSlop(SpecificValues c) {
@@ -19,9 +17,8 @@ public class Values {
 		case SLOP_POSITIONING:
 			path = "Positioning Slop";
 		case SLOP_SHOOTER:
-			path = "Shooter angle slop";
+			path = "Shooter Angle Slop";
 		}
-
 		return getGenericDouble(path);
 	}
 
