313e29725bc9e16e69c6f0c874a9e9f3e54844cf
Ben Klein <robobenklein@gmail.com>
2016-02-16 18:41:40 -0500

    Redo kickout

--- src/main/java/frc/team3966/toastrhino/commands/KickOut.java
+++ src/main/java/frc/team3966/toastrhino/commands/KickOut.java
@@ -1,7 +1,6 @@
 package frc.team3966.toastrhino.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import frc.team3966.toastrhino.RobotModule;
 
 /**
@@ -10,9 +9,7 @@ import frc.team3966.toastrhino.RobotModule;
 public class KickOut extends Command {
 
   public static final double maxTime = .25;//max time, in seconds
-  public static final double converFactor = Math.pow(10, -9);
-  public boolean doThis = true;
-  public double time = 0;
+  public static final double convertFactor = Math.pow(10, 9);
   private long startTime = 0; //dont touch :P
 
   public KickOut() {
@@ -21,29 +18,23 @@ public class KickOut extends Command {
 
   // Called just before this Command runs the first time
   protected void initialize() {
-	  doThis = true;
-	  startTime = System.nanoTime();
+    startTime = System.nanoTime();
   }
 
   // Called repeatedly when this Command is scheduled to run
   protected void execute() {
-    long ctime = System.nanoTime();
-    time = (ctime - startTime) * converFactor;
-    if (time >= maxTime) {
-    	return;
-    }
-    if (doThis) {
-    	//your code does this
-    }
+    // Do some stuff
+    //arm.kickout or something
   }
 
   // Make this return true when this Command no longer needs to run execute()
   protected boolean isFinished() {
-    return !doThis;
+    return (System.nanoTime() > (startTime + (maxTime * convertFactor)));
   }
 
   // Called once after isFinished returns true
   protected void end() {
+    // Set things to zero.
   }
 
   // Called when another command which requires one or more of the same
