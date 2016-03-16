5869767308be7dc53a345f56b77947c8b9455751
Ben Klein <robobenklein@gmail.com>
2016-03-16 18:43:22 -0400

    Try/Catch when AnalogInput is not linked

--- src/main/java/frc/team3966/toastrhino/subsystems/ArmBallGrab.java
+++ src/main/java/frc/team3966/toastrhino/subsystems/ArmBallGrab.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team3966.toastrhino.RobotMap;
+import frc.team3966.toastrhino.RobotModule;
 
 /**
  *
@@ -15,37 +16,50 @@ public class ArmBallGrab extends Subsystem {
 
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  
+
   VictorSP SLmotor = new VictorSP(RobotMap.SLmotor);
   VictorSP SRmotor = new VictorSP(RobotMap.SRmotor);
-  
-  AnalogInput SLinput = new AnalogInput(0);
-  AnalogInput SRinput = new AnalogInput(1);
-  
-  AnalogTrigger SLtrigger = new AnalogTrigger(SLinput);
-  AnalogTrigger SRtrigger = new AnalogTrigger(SRinput);
-  
-  Counter SLcounter = new Counter(SLtrigger);
-  Counter SRcounter = new Counter(SRtrigger);
 
-  public ArmBallGrab() {
-    
-  }
+  boolean Scounters = false;
+
+  AnalogInput SLinput;
+  AnalogInput SRinput;
+  AnalogTrigger SLtrigger;
+  AnalogTrigger SRtrigger;
+  Counter SLcounter;
+  Counter SRcounter;
   
+  
+  public ArmBallGrab() {
+    try {
+      SLinput = new AnalogInput(0);
+      SRinput = new AnalogInput(1);
+      SLtrigger = new AnalogTrigger(SLinput);
+      SRtrigger = new AnalogTrigger(SRinput);
+      SLcounter = new Counter(SLtrigger);
+      SRcounter = new Counter(SRtrigger);
+      Scounters = true;
+    } catch (UnsatisfiedLinkError e) {
+      RobotModule.logger.warn("Shooter Counters no Link");;
+    }
+  }
+
   public void dash_all() {
     SmartDashboard.putData("SLmotor", SLmotor);
     SmartDashboard.putData("SRmotor", SRmotor);
-    SmartDashboard.putData("SLinput", SLinput);
-    SmartDashboard.putData("SRinput", SRinput);
-    SmartDashboard.putData("SLcounter", SLcounter);
-    SmartDashboard.putData("SRcounter", SRcounter);
+    if (Scounters) {
+      SmartDashboard.putData("SLinput", SLinput);
+      SmartDashboard.putData("SRinput", SRinput);
+      SmartDashboard.putData("SLcounter", SLcounter);
+      SmartDashboard.putData("SRcounter", SRcounter);
+    }
   }
 
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     //setDefaultCommand(new MySpecialCommand());
   }
-  
+
   public void grab(double speed) {
     SLmotor.set(-speed);
     SRmotor.set(speed);
