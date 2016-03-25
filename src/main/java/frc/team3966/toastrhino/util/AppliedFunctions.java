2824b1c9c5dea538223c201f1c6179f9dd205c52
Cade Brown <brown.cade@gmail.com>
2016-03-24 22:03:34 -0500

    Fixed arm constants

--- src/main/java/frc/team3966/toastrhino/util/AppliedFunctions.java
+++ src/main/java/frc/team3966/toastrhino/util/AppliedFunctions.java
@@ -8,8 +8,8 @@ public class AppliedFunctions {
 	/* constants */
 	public final static float gravity = 9.81f; // m per s^2
 	public final static float goalHeight = 1.905f; // m //Should be 2.159. Our test goal is 1.905 m, change this at competition
-	public final static double maxShooterEncoderInput = 100;
-	public final static double minShooterEncoderInput = 2;
+	public final static double maxShooterEncoderInput = 80;
+	public final static double minShooterEncoderInput = 0;
 	public final static double degreesAtEncoderMin = 135.5;
 	public final static double degreesAtEncoderMax = -17;
 	
