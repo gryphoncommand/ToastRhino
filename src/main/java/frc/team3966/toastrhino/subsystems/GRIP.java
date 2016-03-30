038fb4bf4ff2bb464cf1cad9c9e4c420bff21b50
Ben Klein <robobenklein@gmail.com>
2016-03-30 18:16:26 -0400

    Prevent using deprecated methods

--- src/main/java/frc/team3966/toastrhino/subsystems/GRIP.java
+++ src/main/java/frc/team3966/toastrhino/subsystems/GRIP.java
@@ -9,16 +9,18 @@ import frc.team3966.toastrhino.RobotModule;
  *
  */
 public class GRIP extends Subsystem {
-	
-	private double lastGoodValue = 100.0;
+
+  private double lastGoodValue = 100.0;
 
   // What is GRIP Table?
   // Init table:
-	  NetworkTable GRIPtable;
+  NetworkTable GRIPtable;
   
-	  public GRIP() {
-		  GRIPtable = NetworkTable.getTable("GRIP/goal");
-	  }
+  private double[] blank = new double[0];
+
+  public GRIP() {
+    GRIPtable = NetworkTable.getTable("GRIP/goal");
+  }
 
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
@@ -27,22 +29,23 @@ public class GRIP extends Subsystem {
     // Set the default command for a subsystem here.
     //setDefaultCommand(new MySpecialCommand());
   }
-  
+
   public double getCenterY() {
     // Load Grip values from the network table
-	  double[] GRIPcenterY = GRIPtable.getNumberArray("centerY");
-	  double centerY = 0;
-	  if (GRIPcenterY.length == 1) {
-		  centerY = GRIPcenterY[0];
-	  } else if (GRIPcenterY.length > 1) {
-		  for (double i : GRIPcenterY) {
-			  centerY += i;
-		  }
-		  centerY /= (double)GRIPcenterY.length;
-		  lastGoodValue = centerY;
-	  } else {
-		  return lastGoodValue;
-	  }
+    double[] GRIPcenterY = GRIPtable.getNumberArray("centerY", blank);
+    
+    double centerY = 0;
+    if (GRIPcenterY.length == 1) {
+      centerY = GRIPcenterY[0];
+    } else if (GRIPcenterY.length > 1) {
+      for (double i : GRIPcenterY) {
+        centerY += i;
+      }
+      centerY /= (double)GRIPcenterY.length;
+      lastGoodValue = centerY;
+    } else {
+      return lastGoodValue;
+    }
     /*
     if (one value in array) {
       return thatvalue;
@@ -57,7 +60,7 @@ public class GRIP extends Subsystem {
     } else if (multiple values in array) {
       return average(values);
     }
-    
+
      */
     return centerY; // actualvalue pls
   }
